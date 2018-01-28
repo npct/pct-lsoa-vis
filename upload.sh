10eeeccb50a7354eeb9018e528e64c98419252a0
Nikolai B <nikolai-b@users.noreply.github.com>
2018-01-28 19:32:06 +0000

    Copy files from old repo
    https://github.com/npct-archive/pct-lsoa-vis

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+UPLOAD_DIR=$1
+
+echo "Compressing the (non blank) tiles before uploading to the server\n"
+rm -f ${UPLOAD_DIR}.tar.gz
+
+# Tiles of size 334 bytes are empty so we don't need to transfer them
+find . -name "*.png" -size +335c -type f | tar -czf ${UPLOAD_DIR}.tar.gz -T -
+
+echo "Uploading the tiles to the tileserver\n"
+scp ${UPLOAD_DIR}.tar.gz root@npttile.vs.mythic-beasts.com:/var/www/html/
+rm -f ${UPLOAD_DIR}.tar.gz
+
+echo "Decompressing the tiles on the tileserver\n"
+ssh root@npttile.vs.mythic-beasts.com "tar -xzf /var/www/html/${UPLOAD_DIR}.tar.gz -C /var/www/html"
